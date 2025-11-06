b9fed49af10ebaff352839e20cce6740c0afc4a5
DavidKN10 <davidrs23178@gmail.com>
2025-11-06 14:50:20 -0600

    added alert and output updated

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-function init(){
+function handleClick() {
+    let entryInput = document.getElementById("entryinput");
+    let output = document.getElementById("textoutput"); 
+    output.innerHTML = entryInput.value;
+    alert("David: " + entryInput.value);
+}
+
+function init() {
+    let entryButton = document.getElementById("entrybutton");
+    entryButton.addEventListener('click', handleClick);
 }
  
-
 window.addEventListener('load', init);
